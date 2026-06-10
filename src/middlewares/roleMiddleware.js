e30d4a0bd0a68f8a99278b05ac63b5cf2ccfd7fc
Brian <brianesao@gmail.com>
2026-06-10 17:13:58 -0300

    fix: corrige comparação de perfil para case-insensitive

--- src/middlewares/roleMiddleware.js
+++ src/middlewares/roleMiddleware.js
@@ -1,14 +1,12 @@
 function roleCheck(perfilPermitido) {
-    return function (request, response, next) {
-        if (request.user.perfil === perfilPermitido) {
-            return next();
-        }
-
-        return response.status(403).json({
-            success: false,
-            message: "acesso negado",
-        });
-    };
+  return function (request, response, next) {
+    if (request.user.perfil.toUpperCase() === perfilPermitido) {
+      return next();
+    }
+    return response.status(403).json({
+      success: false,
+      message: "acesso negado",
+    });
+  };
 }
-
 export default roleCheck;
